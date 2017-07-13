5a996aa58a57ca5f5aa074a22affa871a000c3ba
Tink Zhang <zhangyunfengzju@gmail.com>
2017-07-13 17:40:29 +0800

    init notification

--- mobile/src/main/java/com/bullest/waterkeeper/HomeActivity.java
+++ mobile/src/main/java/com/bullest/waterkeeper/HomeActivity.java
@@ -1,11 +1,19 @@
 package com.bullest.waterkeeper;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.RemoteInput;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -386,6 +394,50 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.O
 //        DatabaseReference database = FirebaseDatabase.getInstance().getReference("users");
 //        database.child("user").child(uid).setValue(uid);
         updateDailyProgress();
+        updateNotification();
+    }
+
+    private void updateNotification() {
+        AlarmManager alarmMgr;
+        PendingIntent alarmIntent;
+
+        Intent intent = new Intent(this, HomeActivity.class);
+
+        alarmMgr = (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        alarmIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, intent, 0);
+
+        alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 60*1000, alarmIntent);
+
+        NotificationManager notificationManager = (NotificationManager) HomeActivity.this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        final String KEY_TEXT_REPLY = "key_text_reply";
+        RemoteInput remoteInput = new RemoteInput.Builder(KEY_TEXT_REPLY)
+                .setLabel("Log")
+                .build();
+        NotificationCompat.Action action =
+                new NotificationCompat.Action.Builder(R.drawable.ic_notifications_black_24dp, "Hello", alarmIntent)
+                .addRemoteInput(remoteInput)
+                .build();
+
+        Notification notification = new NotificationCompat.Builder(HomeActivity.this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("Ticker Message")
+                .setContentTitle("Time to Drink")
+                .setContentText("Please drink a lot")
+                .setContentIntent(alarmIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .setColor(getResources().getColor(R.color.colorPrimary))
+                .addAction(action)
+                .build();
+
+        notificationManager.notify(0, notification);
+
+
+
     }
 
     @Subscribe(threadMode = ThreadMode.ASYNC)
